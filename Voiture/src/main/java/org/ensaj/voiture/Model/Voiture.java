ff8a88808bf7d3770f171f60c84a249b0d0f5cea
najiaokacha <100485014+najiaokacha@users.noreply.github.com>
2023-12-09 12:45:02 +0100

    Update Voiture.java

--- Voiture/src/main/java/org/ensaj/voiture/Model/Voiture.java
+++ Voiture/src/main/java/org/ensaj/voiture/Model/Voiture.java
@@ -1,16 +1,8 @@
 package org.ensaj.voiture.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.Transient;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Data @AllArgsConstructor
@@ -24,20 +16,47 @@ public class Voiture {
     private String matricule;
     private String marque;
     private String model;
-    @ManyToOne
+
+    @ManyToOne()
     @JoinColumn(name = "client_id")
     private Client client;
 
 
 
-    public Voiture(String matricule, String marque, String model, client cllient) {
-        this.matricule = matricule;
-        this.marque = marque;
-        this.model = model;
-        this.client = client;
-    }
 
     public Voiture() {
 
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
     }
 }
