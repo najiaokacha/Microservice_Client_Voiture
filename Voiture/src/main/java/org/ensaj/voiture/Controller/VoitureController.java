80677799b8058b978fbae827ecb7386fc297b710
najiaokacha <100485014+najiaokacha@users.noreply.github.com>
2023-12-09 12:42:34 +0100

    Update VoitureController.java

--- Voiture/src/main/java/org/ensaj/voiture/Controller/VoitureController.java
+++ Voiture/src/main/java/org/ensaj/voiture/Controller/VoitureController.java
@@ -6,20 +6,20 @@ import org.ensaj.voiture.Model.Client;
 import org.ensaj.voiture.Model.Voiture;
 import org.ensaj.voiture.Repository.VoitureRepository;
 import org.ensaj.voiture.Service.VoitureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
 public class VoitureController {
-
-
+    @Autowired
     VoitureRepository voitureRepository;
-
-
+    @Autowired
     VoitureService voitureService;
-
+    @Autowired
     VoitureClient clientRest;
 
     public VoitureController(VoitureRepository voitureRepository, VoitureService voitureService, VoitureClient clientRest) {
@@ -28,25 +28,18 @@ public class VoitureController {
         this.clientRest = clientRest;
     }
 
-    @GetMapping(value ="/voitures")
-    public List<Voiture> chercherVoiture(){
-        return voitureRepository.findAll();
+    @GetMapping(value = "/voitures", produces = {"application/json"})
+    public ResponseEntity<Object> chercherVoiture() {
+        List<Voiture> voitures = voitureRepository.findAll();
+        return ResponseEntity.ok(voitures);
     }
 
     @GetMapping("/voitures/{Id}")
-    public Voiture chercherUneVoiture(@PathVariable Long Id) throws Exception{
-        Voiture voiture = voitureRepository.findById(Id).get();
-        Client client = clientRest.findClientByID(voiture.getClientId());
+    public ResponseEntity<Object> chercherUneVoiture(@PathVariable Long Id) throws Exception {
+        Voiture voiture = voitureRepository.findById(Id)
+                .orElseThrow(() -> new Exception("Voiture Introuvable"));
+        Client client = clientRest.findClientByID(voiture.getClient().getId());
         voiture.setClient(client);
-        return voiture;
+        return ResponseEntity.ok(voiture);
     }
-
-
-    @PostMapping("/voitures")
-    public Voiture enregistrerUneVoiture(@RequestBody Voiture voiture){
-        return voitureService.enregistrerVoiture(voiture);
-    }
-
-
-
 }
